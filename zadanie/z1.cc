ea977489c6054ce305a35128e7042b6ed0312543
nito <nito@STUD-3A-PC14.kl9.uspu.ru>
2020-09-21 17:30:06 +0500

    stoneee

--- zadanie/z1.cc
+++ zadanie/z1.cc
@@ -1,6 +1,29 @@
 #include <iostream>
 #include <random>
 #include <chrono>
+#include <sstream>
+
+
+int read_int(
+		const char*question)
+{
+int result;
+std::string inp;
+re:std::cout<<question;
+std::getline(std::cin, inp);
+{
+std::stringstream
+inps{inp};
+inps>>result;
+if (inps.fail()){
+	std::cout<<"Некорректный ввод!"<<std::endl;
+	goto re;
+}
+}
+return result;
+}
+
+
 
 int main (){
 	using clk=std::chrono::system_clock;
@@ -8,9 +31,27 @@ int main (){
 		unsigned(clk::now().time_since_epoch().count())
 	};
 	std::uniform_int_distribution<> d{10,30};
+	unsigned stone_cout=d(rnd);
+	int user_turn =
+			read_int("Сколько камней"
+					"Берем (1-3)? ");
 	for(unsigned i=0;
 			i<10;++i)
 		std::cout<<
 		d(rnd) <<' ';
+
+/*
+	int i;
+	std::string inp;
+	reee:std::getline(std::cin ,inp);
+	std::stringstream inps{inp};
+	inps >>i;
+
+	if(inps.fail()){
+		std::cout<<"Aaa! \n";
+		goto reee;
+	}
+
+	*/
 	return 0;
 }
